2d78cc6421b7b995873db3d7e545f11343c7e454
Justin-Wang2-ICD201-01 <justin.wang@student.tdsb.on.ca>
2024-11-05 17:37:04 -0500

    Update PP2_1.py

--- PP2_1.py
+++ PP2_1.py
@@ -2,15 +2,14 @@
 
 def q1(): 
   num = int(input("please enter a number: "))
-  if (num % 2) == 0:
+  if num % 2 == 0:
     print (f"{num} is even")
-  if (num % 2) > 0:
-    print(f"{num} is odd")
-  if (num % 2) < 0:
+  if(num % 2 != 0:
     print(f"{num} is odd")
 
+
 def q2(): 
-name = str(input("enter something: "))
+  name = str(input("enter something: "))
   if name == "Kalisz":
     print("teacher")
   if name != "Kalisz":
